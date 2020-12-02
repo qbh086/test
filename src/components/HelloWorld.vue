78c4c1907c0cd1294f1ba3d5170772b68b484181
qbh086 <1299234436@qq.com>
2020-12-02 16:14:29 +0800

    添加了md编辑器

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,48 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div id="layout">
+    <div id="blog_editormd" style="margin-top: 5px;">
+      <textarea style="display: none;">[TOC]</textarea>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
+  export default {
+    data() {
+      return {
+        blogEditor: {}
+      }
+    },
+    methods: {
+
+    },
+    created() {
+      var _this = this;
+      _this.$nextTick(() => {
+        const blogEditor = editormd("blog_editormd", {
+          placeholder: '欢迎使用editor.md 编辑器',
+          width: "100%",
+          height: "880",
+          syncScrolling: "single",
+          emoji: true,
+          path: '/static/editor.md-master/lib/',
+          saveHTMLToTextarea: true,
+          tocm: true,
+          tex: true,
+          flowChart: true,
+          imageUpload: true,
+          imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+          imageUploadURL: "",
+          //crossDomainUpload : true
+          previewTheme: "dark"
+          /**设置主题颜色*/
+          /* editorTheme: "pastel-on-dark",
+           theme: "gray",
+           previewTheme: "dark"*/
+        });
+        //将刚刚定义的对象 存到vue中，用的时候再取出来
+        _this.blogEditor = blogEditor;
+      });
     }
   }
-}
 </script>
